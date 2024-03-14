7c9bfdf16e30097651a93b5d36274f00eac20546
dakshpunia <dakshpunia106@gmail.com>
2024-03-14 19:20:54 +0530

    "JUst some random commit"

--- hi.py
+++ hi.py
@@ -1,2 +1,5 @@
 name,age=input('What\'s your name?'),int(input("What\'s your age?"))
 print(f'Hello!{name}, i know you were born in the year{2024-age} and you are a gem of a person. Nice time meeting you, See you soon!!!')
+while age>10:
+    print(age)
+    age*=3
