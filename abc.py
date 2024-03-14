ccba75287d02804072aaa15cba3b4566a963d172
dakshpunia <dakshpunia106@gmail.com>
2024-03-14 19:01:39 +0530

    Removing fab from abc

--- abc.py
+++ abc.py
@@ -2,16 +2,8 @@ a,b,c=int(input()),int(input()),int(input())
 h=0
 if a>b and a>c:
     print(f'{a}is the greates among{a,b,c}')
-    h=a
 elif b>a and b>c:
     print(f'{b}is the greates among{a,b,c}')
-    h=b
 else:    
     print(f'{c}is the greates among{a,b,c}')
-    h=c
 
-x,y=1,1
-print(x)
-for i in range(h-1):
-    print(y)
-    x,y=y,x+y
