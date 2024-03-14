1cbbbadc10efe357788bf73aca9ba152eb46e11d
dakshpunia <dakshpunia106@gmail.com>
2024-03-14 18:47:38 +0530

    Adding hi.py

---
+++ hi.py
@@ -0,0 +1,2 @@
+name,age=input('What\'s your name?'),int(input("What\'s your age?"))
+print(f'Hello!{name}, i know you were born in the year{2024-age} and you are a gem of a person. Nice time meeting you, See you soon!!!')
